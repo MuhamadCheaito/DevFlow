ba6536415e4dd7f1c45ff4c877791c7f26005a46
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2025-09-24 20:59:40 +0300

    intial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js </h1>
+    </div>
+  )
+}
+
+export default Home
